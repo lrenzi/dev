f30ce374ac7c6549973865ecc14b2a87f4e9c7ae
Glaucia Rangel <glaucia.rangel@gmail.com>
2016-12-14 15:01:01 -0200

    subindo web site

---
+++ BSI.GestDoc.Website/Scripts/Controllers/DocumentoController.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+app.controller("DocumentoController", ["$scope", "$routeParams", "$location", "FileUploadService", function ($scope, $routeParams, $location, FileUploadService) {
+    $scope.ConsultarArquivos = function () {
+        $scope.listaDocumentosClienteTipo = FileUploadService.RetornarDocumentoClienteTipo().then(function (data) {
+            $scope.listaDocumentosClienteTipo = data;
+        });
+    };
+    $scope.EnviarArquivos = function () {
+        $scope.listaDocumentosClienteTipo = FileUploadService.EnviarArquivos().then(function (data) {
+            $scope.listaDocumentosClienteTipo = data;
+        });
+    };
+}]);
