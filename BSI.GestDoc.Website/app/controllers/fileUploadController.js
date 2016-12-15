7005503488faf2d362a8518a54ef176fcc6d1ee7
Glaucia Rangel <glaucia.rangel@gmail.com>
2016-12-15 19:45:18 -0200

    checkin

--- BSI.GestDoc.Website/app/controllers/fileUploadController.js
+++ BSI.GestDoc.Website/app/controllers/fileUploadController.js
@@ -1,15 +1,56 @@
 ﻿'use strict';
-app.controller("fileUploadController", ["$scope", "$routeParams", "$location", "FileUploadService", function ($scope, $routeParams, $location, fileUploadService) {
-    alert("nois");
+app.controller("fileUploadController", ["$scope", "$routeParams", "$location", "fileUploadService", function ($scope, $routeParams, $location, fileUploadService) {
+    //alert("nois");
+
+    $scope.model = {};
+    $scope.selectedFile = [];
+    $scope.uploadProgress = 0;
+
     $scope.ConsultarArquivos = function () {
-        debugger;
+        //debugger;
         $scope.listaDocumentosClienteTipo = fileUploadService.RetornarDocumentoClienteTipo().then(function (data) {
             $scope.listaDocumentosClienteTipo = data;
+
+           
+
         });
     };
     $scope.EnviarArquivos = function () {
-        $scope.listaDocumentosClienteTipo = fileUploadService.EnviarArquivos().then(function (data) {
+        $scope.listaDocumentosClienteTipo = null;
+
+
+        debugger;
+
+       
+
+        debugger;
+
+        var file = $scope.selectedFile[0];
+        $scope.upload = $upload.upload({
+            url: 'http://localhost:44857/api/FileUpload/EnviarArquivos',
+            method: 'POST',
+            data: angular.toJson($scope.model),
+            file: file
+        }).progress(function (evt) {
+            debugger;
+            $scope.uploadProgress = parseInt(100.0 * evt.loaded / evt.total, 10);
+        }).success(function (data) {
+            debugger;
+            //do something
+        }).error(function (data, status) {
+            debugger;
+        })
+
+        alert("k");
+      
+        $scope.onFileSelect = function ($files) {
+            $scope.uploadProgress = 0;
+            $scope.selectedFile = $files;
+        };
+            
+        /*
+        fileUploadService.EnviarArquivos().then(function (data) {
             $scope.listaDocumentosClienteTipo = data;
-        });
+        });*/
     };
 }]);
---
+++ BSI.GestDoc.Website/app/controllers/fileUploadTesteController.js
@@ -0,0 +1,48 @@
+﻿'use strict';
+debugger;
+app.controller('uploadCtrl', [
+        '$scope',
+        '$upload',
+        function ($scope, $upload) {
+
+            debugger;
+
+            $scope.model = {};
+            $scope.selectedFile = [];
+            $scope.uploadProgress = 0;
+
+            $scope.uploadFile = function () {
+                debugger;
+                var file = $scope.selectedFile[0];
+                $scope.upload = $upload.upload({
+                    url: 'http://localhost:44857/api/FileUpload/EnviarArquivos',
+                    method: 'POST',
+                    data: angular.toJson($scope.model),
+                    file: file
+                }).progress(function (evt) {
+                    debugger;
+                    $scope.uploadProgress = parseInt(100.0 * evt.loaded / evt.total, 10);
+                }).success(function (data) {
+                    debugger;
+                }).error(function (data, status) {
+                    debugger;
+                });
+            };
+
+            $scope.onFileSelect = function ($files) {
+                $scope.uploadProgress = 0;
+                $scope.selectedFile = $files;
+            };
+        }
+    ])
+    .directive('progressBar', [
+        function () {
+            return {
+                link: function ($scope, el, attrs) {
+                    $scope.$watch(attrs.progressBar, function (newValue) {
+                        el.css('width', newValue.toString() + '%');
+                    });
+                }
+            };
+        }
+    ]);
