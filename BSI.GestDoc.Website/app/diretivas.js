1d824f246de1a09eb687a0d2e61c08563a216a8f
Glaucia Rangel <glaucia.rangel@gmail.com>
2017-01-03 13:34:33 -0200

    - Corrigido erro de português - Adicionado diretiva para tratamento de campos alpha-Numericos - Removida as diretivas do app.js e criado o diretivas.js para adicionar as diretivas.

---
+++ BSI.GestDoc.Website/app/diretivas.js
@@ -0,0 +1,44 @@
+﻿app.directive('titulos', function () {
+    return {
+        template: '<h2>{{titulo}}</h2> <h4>{{subTitulo}}</h4>'
+    };
+});
+
+app.directive('alphaNumericos', function () {
+    return {
+        retrict: 'A',
+        link: function ($scope, elem, attr, model) {
+            elem.on('keypress', function (e) {
+                var charCode = (e.which) ? e.which : e.keyCode;
+                var str = String.fromCharCode(charCode);
+                var expressao = /[a-zA-Z0-9]/g;
+                var reg = new RegExp(expressao);
+                if (!reg.test(str)) {
+                    e.preventDefault();
+                    return false;
+                }
+                return true;
+            }).on('focusout', function (e) {
+                this.value = this.value.replace(/[^a-zA-Z0-9]/g, '')
+            }).on('paste', function (e) {
+                var campo = this;
+                setTimeout(function () {
+                    campo.value = campo.value.replace(/[^a-zA-Z0-9]/g, '');
+                });
+            });
+        }
+    }
+});
+
+app.directive("focusOn", function ($timeout) {
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            scope.$on(attrs.focusOn, function (e) {
+                $timeout((function () {
+                    element[0].focus();
+                }), 10);
+            });
+        }
+    };
+});
