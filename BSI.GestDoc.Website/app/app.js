cdab6b10fea9e27dcef58860c8f51b6f503356bc
Wesley W Kurten BSI <wesley.kurten@bsitecnologia.com.br>
2017-01-04 10:39:23 -0200

    inclusão da coluna perfil no filtro

--- BSI.GestDoc.Website/app/app.js
+++ BSI.GestDoc.Website/app/app.js
@@ -59,19 +59,31 @@ app.config(function ($routeProvider) {
 
 app.filter('customFilter', function () {
     return function (items, filter, columns) {
-        
         if (filter == "" || filter == undefined) {
             return items;
         }
         var exists = false;
         var filtered = [];
+        var nameColumn = "";
+        var propertyColum = [];
+        var valuePropoertyColum;
         columns = columns.split("|");
         for (var i = 0; i < items.length; i++) {
             exists = false;
             for (var j = 0; j < columns.length; j++) {
                 if (!exists) {
-                    if (items[i][columns[j]] != undefined) {
-                        if (items[i][columns[j]].indexOf(filter) > -1) {
+                    nameColumn = columns[j];
+                    if (nameColumn.indexOf(".") > -1) {
+                        propertyColum = nameColumn.split(".");
+                        valuePropoertyColum = items[i][propertyColum[0]];
+                        for (var k = 1; k < propertyColum.length; k++) {
+                            valuePropoertyColum = valuePropoertyColum[propertyColum[k]];
+                        }
+                    } else {
+                        valuePropoertyColum = items[i][nameColumn];
+                    }
+                    if (valuePropoertyColum != undefined) {
+                        if (valuePropoertyColum.indexOf(filter) > -1) {
                             filtered.push(items[i]);
                             exists = true;
                         }
