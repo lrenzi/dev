6c541dfd8bfb0e53b551e8268dd0b43759cdc925
desenv02 <desenv02@BSIW12DEV02>
2017-01-06 16:13:41 -0200

    ajustando merge

--- BSI.GestDoc.Website/app/services/listaPropostasService.js
+++ BSI.GestDoc.Website/app/services/listaPropostasService.js
@@ -1,35 +1,29 @@
 ﻿
 app.service("listaPropostasService", ["$http", "$q", 'ngAuthSettings', 'documentoCliente', 'localStorageService', function ($http, $q, ngAuthSettings, documentoCliente, localStorageService) {
 
-    var deferred = $q.defer();
+
     var infClientes = localStorageService.get('ngAuthSettings');
     var serviceBase = ngAuthSettings.apiServiceBaseUri;
     var listaPropostasServiceFactory = {};
+    var _retornarPropostasCliente = function () {
 
-
-    var _retornarPropostasCliente = function (keySearch) {
-        
-        $http.post(serviceBase + "api/Proposta/ListarPropostas?usuarioId=" + infClientes.usuarioId + "&clientId=" + infClientes.clienteId + "&numeroProposta=" + keySearch)
+        return $http.post(serviceBase + "api/Proposta/ListarPropostas?usuarioId=" + infClientes.usuarioId + "&clientId=" + infClientes.clienteId + "&numeroProposta=" + documentoCliente.numeroPesquisaProposta)
         .then(function (response) {
-            debugger
+
             if (typeof response.data === 'object') {
-                //deferred.resolve(response);
                 return response.data;
             } else {
-                return deferred.reject(response);
+                return $q.reject(response.data);
             }
         },
-        function (response) {
-            debugger
-            deferred.reject(response);
-            response.data.message = "Erro ao acessar o serviço de Consulta de Documentos do Cliente.";
+        function (err) {
+            utilService.mensagemAlerta("Erro ao acessar o serviço de Consulta de Documentos do Cliente.")
         });
-        return deferred.promise;
     };
-  
-    
+
+
     listaPropostasServiceFactory.ListarPropostas = _retornarPropostasCliente;
-    
+
     return listaPropostasServiceFactory;
 
 }]);
