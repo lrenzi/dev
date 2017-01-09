dc708284266d5cd6a9be63e85ce499afb6aca0ba
Glaucia Rangel <glaucia.rangel@gmail.com>
2017-01-09 16:22:21 -0200

    ajuste sem #

--- BSI.GestDoc.Website/app/diretivas.js
+++ BSI.GestDoc.Website/app/diretivas.js
@@ -8,6 +8,8 @@ app.directive('alphaNumericos', function () {
     return {
         retrict: 'A',
         link: function ($scope, elem, attr, model) {
+            
+
             elem.on('keypress', function (e) {
                 var charCode = (e.which) ? e.which : e.keyCode;
                 var str = String.fromCharCode(charCode);
@@ -24,6 +26,33 @@ app.directive('alphaNumericos', function () {
                 var campo = this;
                 setTimeout(function () {
                     campo.value = campo.value.replace(/[^a-zA-Z0-9]/g, '');
+                });
+            });
+        }
+    }
+});
+
+app.directive('alphaNumericosEspecial', function () {
+    return {
+        retrict: 'A',
+        link: function ($scope, elem, attr, model) {
+            
+            elem.on('keypress', function (e) {
+                var charCode = (e.which) ? e.which : e.keyCode;
+                var str = String.fromCharCode(charCode);
+                var expressao = /[^#&]/;
+                var reg = new RegExp(expressao);
+                if (!reg.test(str)) {
+                    e.preventDefault();
+                    return false;
+                }
+                return true;
+            }).on('focusout', function (e) {
+                this.value = this.value.replace(/[#&]/, '')
+            }).on('paste', function (e) {
+                var campo = this;
+                setTimeout(function () {
+                    campo.value = campo.value.replace(/[#&]/, '');
                 });
             });
         }
---
+++ BSI.GestDoc.Website/app/filter.js
@@ -0,0 +1,38 @@
+﻿
+app.filter('customFilter', function () {
+    return function (items, filter, columns) {
+        if (filter == "" || filter == undefined) {
+            return items;
+        }
+        var exists = false;
+        var filtered = [];
+        var nameColumn = "";
+        var propertyColum = [];
+        var valuePropoertyColum;
+        columns = columns.split("|");
+        for (var i = 0; i < items.length; i++) {
+            exists = false;
+            for (var j = 0; j < columns.length; j++) {
+                if (!exists) {
+                    nameColumn = columns[j];
+                    if (nameColumn.indexOf(".") > -1) {
+                        propertyColum = nameColumn.split(".");
+                        valuePropoertyColum = items[i][propertyColum[0]];
+                        for (var k = 1; k < propertyColum.length; k++) {
+                            valuePropoertyColum = valuePropoertyColum[propertyColum[k]];
+                        }
+                    } else {
+                        valuePropoertyColum = items[i][nameColumn];
+                    }
+                    if (valuePropoertyColum != undefined) {
+                        if (valuePropoertyColum.indexOf(filter) > -1) {
+                            filtered.push(items[i]);
+                            exists = true;
+                        }
+                    }
+                }
+            }
+        }
+        return filtered;
+    };
+});
