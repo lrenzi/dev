cc354eb38ab10535e35557d7e55944a1a10d85b2
desenv02 <desenv02@BSIW12DEV02>
2017-01-19 16:47:01 -0200

    novo serviço Consulta de Cliente

---
+++ BSI.GestDoc.WebAPI/Controllers/ClienteController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Http;
+using BSI.GestDoc.Entity;
+using BSI.GestDoc.Repository.CRUD;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BSI.GestDoc.WebAPI.Controllers
+{
+
+    [System.Web.Http.RoutePrefix("api/Cliente")]
+    public class ClienteController : ApiController
+    {
+        /// <summary>
+        /// Consulta o cliente na base de dados pelo id
+        /// </summary>
+        /// <param name="clienteId"></param>
+        /// <returns>Cliente</returns>
+        [Authorize]
+        [System.Web.Http.Route("Consultar")]
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Consultar(long clienteId)
+        {
+
+            Cliente ClienteRetorno = null;
+
+            try
+            {
+                ClienteRetorno = new ClienteDal().GetCliente(clienteId);
+
+                return Ok(ClienteRetorno);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.GetBaseException().Message);
+            }
+            finally
+            {
+                this.Dispose();
+            }
+
+        }
+    }
+
+}
